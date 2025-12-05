85eb81c72b7456721482b25bed9ce9bd5e58bf66
0marS3lvas <omarselvas153@gmail.com>
2025-12-05 01:38:20 -0600

    avances

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,15 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './presentation/layout/mainLayout/main-layout';
 import { Dashboard } from './presentation/pages/dashboard/dashboard';
 import { Callback } from './presentation/pages/callback/callback';
-import { ArtistDetail } from './presentation/pages/artist-detail/artist-detail';
 
 export const routes: Routes = [
   {
     path: '',
-    component: MainLayoutComponent, 
+    component: MainLayoutComponent, // El Layout envuelve al Dashboard
     children: [
-      { path: '', component: Dashboard },
-      { path: 'artist/:id', component: ArtistDetail }
+      { path: '', component: Dashboard }
     ]
   },
-  { path: 'callback', component: Callback }, // Callback va fuera del layout (sin sidebar/player)
+  { path: 'callback', component: Callback }, // El Callback va sin layout (pantalla completa)
   { path: '**', redirectTo: '' }
 ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   spotifyClientId: '78bbcc6d2c5c4bd28eea7553b14f595b',
-  spotifyRedirectUri: 'http://localhost:4200/callback',
+  spotifyRedirectUri: 'http://127.0.0.1:4200/callback',
   spotifyScopes: 'user-read-private user-read-email user-modify-playback-state streaming',
   backendUrl: 'http://localhost:3000/api'
 };
